e3c43bfed6ec511d1a69439372b0f88457cd4006
Goldfish808 <tjddns0853@gmail.com>
2022-10-13 12:29:35 +0900

    [stomp- pub/sub] ChatRoom 아이디, 네임 세팅방식 변경

--- src/main/java/site/metacoding/red/web/dto/chat/ChatRoom.java
+++ src/main/java/site/metacoding/red/web/dto/chat/ChatRoom.java
@@ -2,37 +2,50 @@ package site.metacoding.red.web.dto.chat;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import org.springframework.web.socket.WebSocketSession;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import site.metacoding.red.domain.chat.ChatMessage;
 import site.metacoding.red.service.ChatService;
 
 @Getter
+@Setter
 public class ChatRoom {
     private String roomId;
     private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
 
-    @Builder
-    public ChatRoom(String roomId, String name) {
-        this.roomId = roomId;
-        this.name = name;
+    public static ChatRoom create(String name) {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.roomId = UUID.randomUUID().toString();
+        chatRoom.name = name;
+        return chatRoom;
     }
 
-    public void handlerActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
+    // 웹소켓 세션에 들어왔을 때, 클라이언트에게 메시지를 발송하는 구현체
+    // private Set<WebSocketSession> sessions = new HashSet<>();
 
-    }
+    // @Builder
+    // public ChatRoom(String roomId, String name) {
+    // this.roomId = roomId;
+    // this.name = name;
+    // }
 
-    private <T> void sendMessage(T message, ChatService chatService) {
-        sessions.parallelStream()
-                .forEach(session -> chatService.sendMessage(session, message));
-    }
+    // public void handlerActions(WebSocketSession session, ChatMessage chatMessage,
+    // ChatService chatService) {
+    // if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
+    // sessions.add(session);
+    // chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
+    // }
+    // sendMessage(chatMessage, chatService);
+
+    // }
+
+    // private <T> void sendMessage(T message, ChatService chatService) {
+    // sessions.parallelStream()
+    // .forEach(session -> chatService.sendMessage(session, message));
+    // }
 }
